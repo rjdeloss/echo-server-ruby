d90daaeb5f8b6b9b5761e41f2dcebfb024b5e94c
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-16 12:41:16 -0400

    revert to previous changes

--- spec/echo_server_spec.rb
+++ spec/echo_server_spec.rb
@@ -4,7 +4,7 @@ describe EchoServer do
 
     describe ".test" do
         it "basic test for Ruby CI" do 
-            expect(true).to eq(false)
+            expect(true).to eq(true)
         end
     end
 end
