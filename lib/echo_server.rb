41b86de7a55237c9b3eea04b4995d221b699cd57
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-16 11:07:22 -0400

    initial setup for echo server project

---
+++ lib/echo_server.rb
@@ -0,0 +1,2 @@
+class EchoServer
+end
---
+++ spec/echo_server_spec.rb
@@ -0,0 +1,10 @@
+require "echo_server"
+
+describe EchoServer do
+
+    describe ".test" do
+        it "basic test for Ruby CI" do 
+            expect(true).to eq(true)
+        end
+    end
+end
