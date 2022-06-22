0e543ca0900b87569850f9b33457d480e9df0447
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-22 16:48:50 -0400

    WIP echo-server

---
+++ lib/client.rb
@@ -0,0 +1,34 @@
+require 'socket'
+
+class Client
+    def initialize(host_name, port)
+        @host_name = host_name
+        @port = port
+        @server_socket = nil
+    end
+
+    def start
+        open_server_socket
+        
+        while message = gets
+            if message.include?(".exit")
+                disconnect
+            end
+
+            puts message
+        end
+    end
+
+    def open_server_socket
+        @server_socket = TCPSocket.open(@host_name, @port)
+        puts "Socket is connected to server"
+
+    end
+
+    def disconnect
+        @server_socket.close
+    end
+end
+
+client = Client.new('localhost', 3001)
+client.start
--- lib/echo_server.rb
+++ lib/echo_server.rb
@@ -8,11 +8,22 @@ class EchoServer
         @server = nil
     end
 
-    def start()
+    def start
+        open_server_connection
+        accept_incoming_connections
+    end
+
+    def open_server_connection
         @server = TCPServer.open(@port)
         puts "Server is running on port #{@port}"
     end
+
+    def accept_incoming_connections
+        while true
+            server_socket = @server.accept
+        end
+    end
 end
 
-# server = EchoServer.new(3001)
-# server.start
+server = EchoServer.new(3001)
+server.start
