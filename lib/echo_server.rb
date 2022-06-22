43743a0cd8eb2e1ad0fe449297cdce0e1322b395
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-06-22 08:54:18 -0400

    WIP echo server prep

--- lib/echo_server.rb
+++ lib/echo_server.rb
@@ -1,2 +1,20 @@
+require 'socket'
+
+host = 'localhost'
+port = '5000'
+
 class EchoServer
+    @server = null
+
+    def start()
+        @server = TCPServer.open(host, port)
+    end
+
+    def open_server_connection()
+        
+    end
+
+    def disconnect()
+        @server.close()
+    end
 end
--- spec/echo_server_spec.rb
+++ spec/echo_server_spec.rb
@@ -7,4 +7,23 @@ describe EchoServer do
             expect(true).to eq(true)
         end
     end
+
+    describe ".start" do
+        it "should open a server connection" do
+
+        end
+    end
 end
+
+# Steps for Echo Server
+
+# Server
+# 1 - open server connection
+# 2 - open input and output streams
+# 3 - start an input loop 
+# 7 - closes the connection
+
+# Client
+# 4 - Sends message to the server
+# 5 - Receives a message from the server
+# 6 - Disconnects
